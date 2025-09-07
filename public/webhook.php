5903a47abbe6edf4e2d580a980478dbb8c0bf7bb
Dmitry <kor.dima97@mail.ru>
2025-09-07 11:17:00 +0300

    add storage dir

--- public/webhook.php
+++ public/webhook.php
@@ -4,7 +4,7 @@ $input = file_get_contents('php://input');
 $data = json_decode($input, true);
 
 file_put_contents(
-    __DIR__ . '/storage/webhook.log',
+    __DIR__ . '/webhook.log',
     print_r($data, 1) . PHP_EOL . PHP_EOL,
     FILE_APPEND
 );
