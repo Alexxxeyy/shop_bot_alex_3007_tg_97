1b62ae9fa0bdbdc4b7bae9c755cb665589745574
Alex <Dmitrievich_13@mail.ru>
2025-09-14 00:06:29 +0500

    Keyboard fix new

--- public/webhook.php
+++ public/webhook.php
@@ -4,15 +4,18 @@ use app\components\Telegram;
 
 $data = Telegram::getInputData();
 
-if (
-    !isset($data['message']['chat']['id']) ||
-    (string)$data['message']['chat']['id'] !== (string)Telegram::CHAT_ID
-) {
-    http_response_code(200); // Можно явно отвечать OK
-    exit;
+if (isset($data['message'])) {
+    if ($data['message']['chat']['id'] !== Telegram::CHAT_ID) {
+        exit;
+    }
+    Bot::processMessage($data['message']);
+} elseif (isset($data['callback_query'])) {
+    if ($data['callback_query']['chat']['id'] !== Telegram::CHAT_ID) {
+        exit;
+    }
+    Bot::processCallback($data['message']);
 }
 
-// Логирование для отладки
 file_put_contents(
     __DIR__ . '/../storage/logs/webhook.log',
     print_r($data, true) . PHP_EOL . PHP_EOL,
